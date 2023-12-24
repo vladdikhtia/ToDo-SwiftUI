1d326f4b7d4829a808edb488fd87348f3f65c292
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2023-12-24 17:45:24 +0100

    Profile View

--- ToDo/Views/ProfileView.swift
+++ ToDo/Views/ProfileView.swift
@@ -13,10 +13,58 @@ struct ProfileView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
+                }
             }
             .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundStyle(.blue)
+            .frame(width: 150, height: 150)
+            .padding()
+        
+        // Info: Name, Email, Memeber Since
+        VStack(alignment: .leading){
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Memeber Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        
+        // Sign out button
+        Button("Log Out", action: viewModel.logOut)
+            .tint(.red)
+            .padding()
+        
+        Spacer()
     }
 }
 
